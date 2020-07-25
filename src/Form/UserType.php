64d500a634721d8432f0398c53a89d951717fbb1
francoisvaillant <frvaillant@gmail.com>
2020-07-25 14:00:13 +0200

    user crud ready to implement in admin panel

---
+++ src/Form/UserType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    const ROLES = [
+        'Admin' => 'ROLE_ADMIN',
+        'Utilisateur simple' => 'ROLE_USER'
+    ];
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('roles', ChoiceType::class, [
+                'choices' => self::ROLES,
+                'mapped'  => false,
+            ])
+            ->add('password')
+            ->add('firstName')
+            ->add('lastName')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
