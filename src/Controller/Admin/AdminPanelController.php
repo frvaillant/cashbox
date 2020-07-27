3514e0da9bc600175771ead26f6738ad97261edc
francoisvaillant <frvaillant@gmail.com>
2020-07-27 18:40:10 +0200

    added form and search for date

--- src/Controller/Admin/AdminPanelController.php
+++ src/Controller/Admin/AdminPanelController.php
@@ -8,12 +8,12 @@ use App\Repository\ExtractionRepository;
 use App\Repository\PurchaseRepository;
 use App\Repository\PurchaseUnityRepository;
 use App\Repository\RefundRepository;
-use App\Service\DateChecker;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use \DateTime;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use App\Form\DateBoxType;
 
 
 /**
@@ -24,7 +24,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 class AdminPanelController extends AbstractController
 {
     /**
-     * @Route("/admin/panel", name="admin_panel")
+     * @Route("/admin/panel", name="admin_panel", methods={"GET", "POST"})
      */
     public function index(
         CashFundRepository $cashFundRepository,
@@ -32,9 +32,19 @@ class AdminPanelController extends AbstractController
         PurchaseUnityRepository $purchaseUnityRepository,
         CashCountRepository $cashCountRepository,
         ExtractionRepository $extractionRepository,
-        RefundRepository $refundRepository
+        RefundRepository $refundRepository,
+        Request $request
     ) {
+
+
+        $form = $this->createForm(DateBoxType::class);
+        $form->handleRequest($request);
         $today = new DateTime('now');
+        if ($form->isSubmitted() && $form->isValid()) {
+            $today = $form->getData('date')['date'];
+        }
+
+
         $totalAmountToday = $purchaseRepository->getTotalByDay($today);
         $purchasesToday   = $purchaseRepository->findAllByDate($today);
         $purchasesByPayment = $purchaseRepository->getTotalByPaymentModeToday($today);
@@ -51,6 +61,8 @@ class AdminPanelController extends AbstractController
         $isCountOk = ($totalCashInBox + $cashFundRepository->getCashFund() - $todayCashCount === 0.0);
 
         return $this->render('admin_panel/index.html.twig', [
+            'dateform'             => $form->createView(),
+            'date'                 => $today,
             'controller_name'      => 'AdminPanelController',
             'total_today'          => $totalAmountToday,
             'purchases'            => $purchasesToday,
@@ -66,60 +78,5 @@ class AdminPanelController extends AbstractController
             'refunds_noproduct'    => $totalRefundWithoutProduct,
             'refunds_products'     => $totalRefundWithProduct,
         ]);
-    }
-
-
-    /**
-     * @Route("/cashboard/", name="admin_panel_date", methods={"POST"})
-     */
-    public function cashBoardByDate(
-        Request $request,
-        CashFundRepository $cashFundRepository,
-        PurchaseRepository $purchaseRepository,
-        PurchaseUnityRepository $purchaseUnityRepository,
-        CashCountRepository $cashCountRepository,
-        ExtractionRepository $extractionRepository,
-        RefundRepository $refundRepository,
-        DateChecker $dateChecker
-    ) {
-        $date = $request->request->get('date');
-        if($dateChecker::checkDate($date)) {
-            $date = new DateTime($dateChecker::checkDate($date) . 'T00:00:00');
-
-            $totalAmountToday = $purchaseRepository->getTotalByDay($date);
-            $purchasesToday = $purchaseRepository->findAllByDate($date);
-            $purchasesByPayment = $purchaseRepository->getTotalByPaymentModeToday($date);
-            $purchasesByProduct = $purchaseUnityRepository->getTotalByProductToday($date);
-            $cashFund = $cashFundRepository->getCashFundParams();
-            $cashToday = $purchaseRepository->getCurrentCash();
-            $totalExtractions = $extractionRepository->getTotalExtractions($date);
-            $todayCashCount = $cashCountRepository->getTotayCashCount();
-            $totalRefundToday = $refundRepository->getTotalRefundByDate($date);
-            $totalRefundWithoutProduct = $refundRepository->findAllByDateWithoutProduct($date);
-            $totalRefundWithProduct = $refundRepository->findAllByDateWithProduct($date);
-            $totalCashInBox = $cashToday - $totalExtractions - $totalRefundToday;
-
-            $isCountOk = ($totalCashInBox + $cashFundRepository->getCashFund() - $todayCashCount === 0.0);
-
-            return $this->render('admin_panel/cashboard.html.twig', [
-                'controller_name' => 'AdminPanelController',
-                'total_today' => $totalAmountToday,
-                'purchases' => $purchasesToday,
-                'purchases_by_payment' => $purchasesByPayment,
-                'purchases_products' => $purchasesByProduct,
-                'cashfund' => $cashFund,
-                'cash_today' => $cashToday,
-                'extractions' => $totalExtractions,
-                'cash_in_box' => $totalCashInBox + $cashFundRepository->getCashFund(),
-                'today_cash_count' => $todayCashCount,
-                'is_count_ok' => $isCountOk,
-                'total_refund' => $totalRefundToday,
-                'refunds_noproduct' => $totalRefundWithoutProduct,
-                'refunds_products' => $totalRefundWithProduct,
-            ]);
-        } else {
-            $this->addFlash('error', 'Saisissez une date au bon format jj/mm/aaaa');
-            return $this->redirectToRoute('admin_panel');
-        }
     }
 }
---
+++ src/Form/DateBoxType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormTypeInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use \DateTime;
+
+class DateBoxType extends AbstractType implements FormTypeInterface
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date', DateType::class, [
+                'label'=>'Chercher une date',
+                'widget' => 'single_text',
+                'data' => new DateTime(),
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
