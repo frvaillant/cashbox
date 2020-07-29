7a41fb980335b571c0e4b36b996e3f85b96df441
francoisvaillant <frvaillant@gmail.com>
2020-07-30 00:15:37 +0200

    modify migrations

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ then
   composer install
   yarn install
   php bin/console doctrine:database:create
-  php bin/console doctrine:migrations:migrate
+  php bin/console doctrine:migrations:migrate --no-interaction
   yarn encore prod
   echo '*************************************'
   echo '*************************************'
