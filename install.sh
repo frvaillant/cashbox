8375a3fbdabe34c22fa04fb1cbb181155cb8d2cc
Yorick Barbanneau <ephase@xieme-art.org>
2020-08-01 00:10:46 +0200

    Improve install script and make shellcheck happy

--- install.sh
+++ install.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
-echo 'this program suppose you work with mysql installed @127.0.0.1:3306. Say YES to continue'
-read yes
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+CURRENT="$(pwd)"
+
+err () {
+    #Display a message on stderr and exit with the given code
+    # $mess : message to display
+    # $err : error code (default 10)
+    local mess code
+    mess="${1:-"something go wrong"}"
+    code="${2:-"10"}"
+    >&2 printf "ERROR : %s\n" "${mess}"
+    exit "$code"
+}
+printf "Cashbox will be installed in %s\n" "$SCRIPTPATH"
+printf 'this program suppose you work with mysql installed @127.0.0.1:3306. Say YES to continue : '
+read -r yes
 if [ "$yes" = 'YES' ]
 then
-  folder='cashbox'
-  chmod 755 "$folder"
-  touch "$folder"/.env.local
-  echo 'Your SQL login : '
-  read username
-  echo 'Your SQL password : '
-  read -s password
-  echo 'database name : '
-  read databasename
-  setterEnv=APP_ENV=prod
-  setterDb=DATABASE_URL=mysql://$username:$password@127.0.0.1:3306/$databasename
-  echo "$setterEnv" >> "$folder"/.env.local
-  echo "$setterDb" >> "$folder"/.env.local
-  cd "$folder"
-  composer install
-  yarn install
-  php bin/console doctrine:database:create
-  php bin/console doctrine:migrations:migrate --no-interaction
-  yarn encore prod
+  cd "$SCRIPTPATH" || err "Can't cd into ${SCRIPTPATH}"
+  printf "Your SQL login : "
+  read -r username
+  printf "Your SQL password : "
+  read -r -s password
+  printf "\ndatabase name : "
+  read -r databasename
+  setterEnv="APP_ENV=prod"
+  setterDb="DATABASE_URL=mysql://${username}:${password}@127.0.0.1:3306/${databasename}"
+  echo "${setterEnv}" > ".env.local"
+  echo "${setterDb}" >> ".env.local"
+  composer install || err "Is composer alive?"
+  yarn install || err "Error with Yarn"
+  php bin/console doctrine:database:create || err "Error withDoctrine database create"
+  php bin/console doctrine:migrations:migrate --no-interaction || err "Error with Doctrine database migrate"
+  yarn encore prod || err "Error with Yarn"
   echo '*************************************'
   echo '*************************************'
   echo '*              CASHBOX              *'
@@ -42,3 +53,4 @@ then
   echo '*************************************'
   echo '*************************************'
 fi
+exit 0
